1358bd80d4d42552ddd754a288e39e15fc3adc75
Roman445304 <riverall923@gmail.com>
2025-05-20 19:06:06 +0400

    Update ActivityService.java
    update others param

--- src/main/java/com/skills/indicator/Service/ActivityService.java
+++ src/main/java/com/skills/indicator/Service/ActivityService.java
@@ -6,9 +6,7 @@ import com.skills.indicator.Dto.ActivityData;
 import com.skills.indicator.entity.Activity;
 import com.skills.indicator.entity.User;
 import com.skills.indicator.repository.ActivityRepository;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -19,32 +17,39 @@ import java.util.Optional;
 @NoArgsConstructor
 public class ActivityService {
 
-    @Autowired
     public UserService userService;
 
-    @Autowired
     public ActivityRepository activityRepository;
+
+    @Autowired
+    public ActivityService(UserService userService, ActivityRepository activityRepository) {
+        this.userService = userService;
+        this.activityRepository = activityRepository;
+    }
 
     public void saveOrUpdate(String username, ActivityData data) {
 
         Optional <User> userOptional = userService.getUserByUsername(username);
             try {
                 if (userOptional.isPresent()) {
-                    User user = userOptional.get();
-
-                    Activity activity = new Activity();
-                    activity.setEducation(data.getEducation());
-                    activity.setOthers(data.getOthers());
-                    activity.setReading(data.getReading());
-                    activity.setEnglish(data.getEnglish());
-                    activity.setSport(data.getSport());
-                    activity.setUser(user);
+                    Activity activity = getActivity(data, userOptional);
                     activityRepository.save(activity);
                 }
             }
             catch (Exception e) {
                 throw new UsernameNotFoundException("Пользователь " + username + "не найден");
             }
-
         }
+
+    private Activity getActivity(ActivityData data, Optional<User> userOptional) {
+        User user = userOptional.orElseThrow(RuntimeException::new);
+        Activity activity = new Activity();
+        activity.setEducation(data.getEducation());
+        activity.setOthers(data.getOthers() * 10);
+        activity.setReading(data.getReading());
+        activity.setEnglish(data.getEnglish());
+        activity.setSport(data.getSport());
+        activity.setUser(user);
+        return activity;
+    }
 }
