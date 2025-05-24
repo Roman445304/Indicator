591021d25eb778a5d6efb8d6c9ad9682d7a611d6
Roman445304 <riverall923@gmail.com>
2025-05-24 18:30:27 +0400

    Commit with @Data

--- src/main/java/com/skills/indicator/Dto/ActivityData.java
+++ src/main/java/com/skills/indicator/Dto/ActivityData.java
@@ -5,54 +5,16 @@ import lombok.*;
 
 
 
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ActivityData {
 
-    private int sport;
-    private int education;
-    private int others;
-    private int english;
-    private int reading;
+    private Integer sport;
+    private Integer education;
+    private Integer others;
+    private Integer english;
+    private Integer reading;
+    private Integer writeOff;
 
-
-    public int getSport() {
-        return sport;
-    }
-
-    public void setSport(int sport) {
-        this.sport = sport;
-    }
-
-    public int getEducation() {
-        return education;
-    }
-
-    public void setEducation(int education) {
-        this.education = education;
-    }
-
-    public int getOthers() {
-        return others;
-    }
-
-    public void setOthers(int others) {
-        this.others = others;
-    }
-
-    public int getEnglish() {
-        return english;
-    }
-
-    public void setEnglish(int english) {
-        this.english = english;
-    }
-
-    public int getReading() {
-        return reading;
-    }
-
-    public void setReading(int reading) {
-        this.reading = reading;
-    }
 }
