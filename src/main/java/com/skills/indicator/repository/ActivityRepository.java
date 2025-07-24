5eb5f49eee4129c8c65dac3aacc5cdc037c7e5a1
Roman445304 <riverall923@gmail.com>
2025-07-24 21:24:35 +0400

    Update ActivityRepository.java
    added selectAllActivity

--- src/main/java/com/skills/indicator/repository/ActivityRepository.java
+++ src/main/java/com/skills/indicator/repository/ActivityRepository.java
@@ -3,13 +3,19 @@ package com.skills.indicator.repository;
 
 import com.skills.indicator.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface ActivityRepository extends JpaRepository <Activity, Integer> {
 
+    @Query("SELECT (a.sport + a.education + a.others + a.english + a.reading + a.writeOff) " +
+            "FROM Activity a WHERE a.user.id = :userId")
+    List<Integer> findAllActivityByUserId(@Param("userId") Integer userId);
 
 }
